a009c2aca65f973dc14a6145fdcea5d15f767de6
Dan Frank <danthehitman@gmail.com>
2025-04-28 23:16:57 -0600

    Lots of changes from Teej.  Scared of overwrites.

--- nvim-test/init.lua
+++ nvim-test/init.lua
@@ -39,3 +39,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.keymap.set("n", "<space>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+end)
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
