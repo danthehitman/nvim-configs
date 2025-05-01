dd4bbd19a0c3334b03e64391b5371ab7a3057bc5
Dan Frank <danthehitman@gmail.com>
2025-05-01 11:08:26 -0600

    Adding mason tools install.

--- nvim-test/lua/config/plugins/mason.lua
+++ nvim-test/lua/config/plugins/mason.lua
@@ -1,5 +1,34 @@
 return {
-    {
-	"williamboman/mason.nvim"
-    }
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  -- NEW: auto-installer for tools
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- DAP adapters
+        "codelldb", -- LLDB / Rust / C++
+        "cpptools", -- MS cppdbg (if you still want it)
+
+        -- LSP servers (examples)
+        "clangd",
+        "lua-language-server",
+
+        -- Formatters / linters (examples)
+        "clang-format",
+        "stylua",
+      },
+
+      -- run on startup once per day (defaults)
+      auto_update = false,
+      run_on_start = true,
+      start_delay = 3000, -- ms
+    },
+  },
 }
